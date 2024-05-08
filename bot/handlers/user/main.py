2136d8efcf0ba4069c9cd84c91adcb396424101b
CVladim1r <vladimir.973@list.ru>
2024-05-08 18:30:36 +0300

    replace_from_my_templ..

---
+++ bot/handlers/user/main.py
@@ -0,0 +1,6 @@
+from aiogram import Dispatcher
+
+def register_user_handlers(dp: Dispatcher):
+    # todo: register all user handlers
+    pass
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from bot import start_tgbot
+
+if __name__ == '__main__':
+    start_tgbot()
